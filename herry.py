a961e7710b22708a9f18bf627b161d54cdca88b6
Harisakhtar <109353887+Harisakhtar0@users.noreply.github.com>
2023-01-23 13:15:49 +0500

    Create herry.py

---
+++ herry.py
@@ -0,0 +1,11 @@
+import os, platform
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from DM64 import herry_main
+    herry_main()
+
+elif bit == '32bit':
+    from herry import herry_main
+    herry_main()
